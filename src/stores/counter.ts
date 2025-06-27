c81779b97c65384886b24dc068b8bfa3695f1076
logue <logue@hotmail.co.jp>
2025-06-27 21:32:43 +0900

    Update vite. Update dependencies.

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
 
-export const useCounterStore = defineStore('counter', () => {
+export default defineStore('counter', () => {
   const count = ref(0);
   const doubleCount = computed(() => count.value * 2);
   function increment() {
